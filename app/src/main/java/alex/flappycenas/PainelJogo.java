5e98ad15ac74646cd2c3d24eab281d1276933ae7
Babilu <joao.rosario.exb@gmail.com>
2017-02-09 18:26:27 +0000

    *Alterada geração de notas e inimigos e pontuações*

--- app/src/main/java/alex/flappycenas/PainelJogo.java
+++ app/src/main/java/alex/flappycenas/PainelJogo.java
@@ -40,13 +40,12 @@ public class PainelJogo extends SurfaceView implements SurfaceHolder.Callback {
     private long tempoInicio;
     private long tempoFinal;
     private long intervalo;
-    int altura,largura;
-    private Bitmap img[] =  new Bitmap[4];
-    private Bitmap amigo[] =  new Bitmap[1];
+    int altura, largura;
+    private Bitmap img[] = new Bitmap[4];
+    private Bitmap amigo[] = new Bitmap[1];
     private Random r;
     private Paint paint = new Paint();
     private int pontuacao;
-    private Boolean nota = false;
     private Boolean colision = false;
 
 
@@ -54,7 +53,7 @@ public class PainelJogo extends SurfaceView implements SurfaceHolder.Callback {
         super(context);
         // para obter os eventos do dedo
         getHolder().addCallback(this);
-        threadPrincipal = new ThreadPrincipal(getHolder(),this);
+        threadPrincipal = new ThreadPrincipal(getHolder(), this);
         // Falta a imagem
         WindowManager wm = (WindowManager) getContext().getSystemService(Context.WINDOW_SERVICE);
         Display display = wm.getDefaultDisplay();
@@ -66,11 +65,10 @@ public class PainelJogo extends SurfaceView implements SurfaceHolder.Callback {
         pontuacao = 0;
 
 
-
-        Bitmap bg = Bitmap.createScaledBitmap(BitmapFactory.decodeResource(getResources(),R.drawable.estrada),largura,altura,false);
+        Bitmap bg = Bitmap.createScaledBitmap(BitmapFactory.decodeResource(getResources(), R.drawable.estrada), largura, altura, false);
         fundo = new Fundo(bg);
-        bg = Bitmap.createScaledBitmap(BitmapFactory.decodeResource(getResources(),R.drawable.car),largura/5,2*(altura/8),false);
-        jogador = new Jogador(bg,altura/8,largura/4, limiteX, largura);
+        bg = Bitmap.createScaledBitmap(BitmapFactory.decodeResource(getResources(), R.drawable.car), largura / 5, 2 * (altura / 8), false);
+        jogador = new Jogador(bg, altura / 8, largura / 4, limiteX, largura);
 
         inimigos = new ArrayList<Inimigo>();
         amigos = new ArrayList<Amigo>();
@@ -78,11 +76,11 @@ public class PainelJogo extends SurfaceView implements SurfaceHolder.Callback {
 
         tempoInicio = System.currentTimeMillis();
         intervalo = 1500;
-        img[0] = Bitmap.createScaledBitmap(BitmapFactory.decodeResource(getResources(),R.drawable.cone),largura/5,(altura/8),false);
-        img[1] = Bitmap.createScaledBitmap(BitmapFactory.decodeResource(getResources(),R.drawable.caixa),largura/5,(altura/8),false);
-        img[2] = Bitmap.createScaledBitmap(BitmapFactory.decodeResource(getResources(),R.drawable.lixo),largura/5,(altura/8),false);
-        img[3] = Bitmap.createScaledBitmap(BitmapFactory.decodeResource(getResources(),R.drawable.oil),largura/5,(altura/8),false);
-        amigo[0] = Bitmap.createScaledBitmap(BitmapFactory.decodeResource(getResources(),R.drawable.nota),largura/5,(altura/8),false);
+        img[0] = Bitmap.createScaledBitmap(BitmapFactory.decodeResource(getResources(), R.drawable.cone), largura / 5, (altura / 8), false);
+        img[1] = Bitmap.createScaledBitmap(BitmapFactory.decodeResource(getResources(), R.drawable.caixa), largura / 5, (altura / 8), false);
+        img[2] = Bitmap.createScaledBitmap(BitmapFactory.decodeResource(getResources(), R.drawable.lixo), largura / 5, (altura / 8), false);
+        img[3] = Bitmap.createScaledBitmap(BitmapFactory.decodeResource(getResources(), R.drawable.oil), largura / 5, (altura / 8), false);
+        amigo[0] = Bitmap.createScaledBitmap(BitmapFactory.decodeResource(getResources(), R.drawable.nota), largura / 5, (altura / 8), false);
         r = new Random();
         // para que seja este ecra o que esta activo
         setFocusable(true);
@@ -102,100 +100,70 @@ public class PainelJogo extends SurfaceView implements SurfaceHolder.Callback {
     @Override
     public void surfaceDestroyed(SurfaceHolder surfaceHolder) {
         boolean retry = true;
-        while(retry) {
+        while (retry) {
             try {
                 threadPrincipal.setActivo(false);
-                Intent intent = new Intent();
-                intent.putExtra("pontuacao", pontuacao);
-                ((MainActivity) getContext()).setResult(Activity.RESULT_OK, intent);
-                Log.d("pontuacaoafter", "no painel" + String.valueOf(pontuacao));
-                ((MainActivity) getContext()).finish();
-
             } catch (Exception e) {
                 e.printStackTrace();
             }
-            retry= false;
+            retry = false;
         }
 
     }
 
     @Override
     public boolean onTouchEvent(MotionEvent event) {
-        switch (event.getAction()){
-            case ACTION_DOWN :
+        switch (event.getAction()) {
+            case ACTION_DOWN:
                 jogador.mover(event.getX());
                 return true;
             case ACTION_UP:
-               return true;
+                return true;
         }
         return super.onTouchEvent(event);
     }
 
     public void update() {
         tempoFinal = System.currentTimeMillis() - tempoInicio;
+
         if (tempoFinal > intervalo) {
             Bitmap bg;
-            bg = img[r.nextInt(4)];
-            criarInimigo(bg);
-            tempoInicio = System.currentTimeMillis();
+
+
+            if (pontuacao!= 0 && pontuacao%10 == 0) {
+                intervalo = 750;
+                pontuacao++;
+                bg = amigo[0];
+                criarAmigo(bg);
+                tempoInicio = System.currentTimeMillis();
+            } else {
+                bg = img[r.nextInt(4)];
+                criarInimigo(bg);
+                tempoInicio = System.currentTimeMillis();
+            }
+
         }
         fundo.update();
         for (Inimigo i : inimigos) {
             if (!i.getMorto()) {
                 if (i.isFora(altura)) {
                     i.setMorto();
-                        pontuacao++;
-                    }
-                    i.update();
+                    pontuacao++;
                 }
-                if (colisao(i, jogador)) {
-                    threadPrincipal.setActivo(false);
-                    ((MainActivity) getContext()).setResult(MenuActivity.PERDER_JOGO, new Intent().putExtra("pontuacao", pontuacao));
-                    ((MainActivity) getContext()).finish();
-                } else {
-                    Log.d("test", "inimigo" + i.getRect().toString());
-                    Log.d("test", "jogador" + jogador.getRect().toString());
-                    Log.d("stopThread", "nchama");
-                }
+                i.update();
             }
-            jogador.update();
+            if (colisao(i, jogador)) {
+                /*threadPrincipal.setActivo(false);
+                ((MainActivity) getContext()).setResult(MenuActivity.PERDER_JOGO, new Intent().putExtra("pontuacao", pontuacao));
+                ((MainActivity) getContext()).finish();*/
+            } else {
+                Log.d("test", "inimigo" + i.getRect().toString());
+                Log.d("test", "jogador" + jogador.getRect().toString());
+                Log.d("stopThread", "nchama");
+            }
+        }
+        jogador.update();
 //Nivel
-            if (pontuacao == 20 && nota.equals(false)) {
-                nota = true;
-                intervalo = 750;
-                Bitmap bg;
-                bg = amigo[0];
-                criarAmigo(bg);
-            }
-            if (pontuacao == 40 && nota.equals(true)) {
-                nota = false;
-                intervalo = 600;
-                Bitmap bg;
-                bg = amigo[0];
-                criarAmigo(bg);
-                intervalo = 600;
-            }
-        if (pontuacao == 60 && nota.equals(false)) {
-            nota = true;
-            intervalo = 500;
-            Bitmap bg;
-            bg = amigo[0];
-            criarAmigo(bg);
-        }
-        if (pontuacao == 80 && nota.equals(false)) {
-            nota = true;
-            intervalo = 400;
-            Bitmap bg;
-            bg = amigo[0];
-            criarAmigo(bg);
-        }
-        if (pontuacao == 100 && nota.equals(false)) {
-            nota = true;
-            intervalo = 300;
-            Bitmap bg;
-            bg = amigo[0];
-            criarAmigo(bg);
-        }
 
 
         for (Amigo a : amigos) {
@@ -207,30 +175,38 @@ public class PainelJogo extends SurfaceView implements SurfaceHolder.Callback {
                 a.update();
             }
             if (colisaoA(a, jogador)) {
-                if(colision.equals(false)){
-                    colision=true;
-                    pontuacao+=5;
+                if (colision.equals(false)) {
+                    colision = true;
+                    pontuacao += 5;
                 }
 
             }
         }
 
 
-
-
-        }
+    }
 
     private void criarAmigo(Bitmap bg) {
         int x = limiteX / 4 * (r.nextInt(4));
-        amigos.add(new Amigo(x,bg,largura/5,(altura/8)));
+        amigos.add(new Amigo(x, bg, largura / 5, (altura / 8)));
     }
+
 
     //Colisao dos objectos
     private boolean colisao(Inimigo i, Jogador j) {
 
         if (Rect.intersects(i.getRect(), j.getRect())) {
             return true;
-        } else{
+        } else {
+            return false;
+        }
+    }
+
+    private boolean colisaoNotaInimigo(Inimigo i, Amigo a) {
+
+        if (Rect.intersects(i.getRect(), a.getRect())) {
+            return true;
+        } else {
             return false;
         }
     }
@@ -240,22 +216,20 @@ public class PainelJogo extends SurfaceView implements SurfaceHolder.Callback {
 
         if (Rect.intersects(i.getRect(), j.getRect())) {
             return true;
-        } else{
+        } else {
             return false;
         }
     }
-
-
 
 
     @Override
     public void draw(Canvas canvas) {
         if (canvas != null) {
             fundo.draw(canvas);
-            for(Inimigo i : inimigos){
+            for (Inimigo i : inimigos) {
                 i.draw(canvas);
             }
-            for(Amigo a : amigos){
+            for (Amigo a : amigos) {
                 a.draw(canvas);
             }
             jogador.draw(canvas);
@@ -265,7 +239,7 @@ public class PainelJogo extends SurfaceView implements SurfaceHolder.Callback {
             paint.setTextSize(100);
             paint.setTextAlign(Paint.Align.CENTER);
             paint.setFakeBoldText(true);
-            canvas.drawText("" + pontuacao, canvas.getWidth()/2, canvas.getHeight()/2, paint);
+            canvas.drawText("" + pontuacao, canvas.getWidth() / 2, canvas.getHeight() / 2, paint);
         }
     }
 
@@ -273,9 +247,9 @@ public class PainelJogo extends SurfaceView implements SurfaceHolder.Callback {
         this.threadPrincipal.setActivo(false);
     }
 
-    public void criarInimigo(Bitmap bg){
+    public void criarInimigo(Bitmap bg) {
         int x = limiteX / 4 * (r.nextInt(4));
-        inimigos.add(new Inimigo(x,bg,largura/5,(altura/8)));
+        inimigos.add(new Inimigo(x, bg, largura / 5, (altura / 8)));
     }
 
 }
